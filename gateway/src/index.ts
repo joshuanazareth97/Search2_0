f1c82b7a95e6b973ce20a1518ddf71d7b48bca56
Joshua <joshuanazareth97@gmail.com>
2022-03-18 01:17:50 +0530

    add additional logging

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -4,10 +4,10 @@ import server from "./server";
 
 // Constants
 const serverStartMsg = "Express server started on port: ",
-  port = process.env.NODE_DOCKER_PORT || 3001;
+  port = process.env.NODE_DOCKER_PORT;
 //   port = 3001;
 
 // Start server
 server.listen(port, () => {
-  logger.info(serverStartMsg + port);
+  logger.info(serverStartMsg + port + ":" + process.env.NODE_LOCAL_PORT);
 });
